6db8f616fd55537a30b808ca8650581df4c030c8
Sean Mussenden <smussenden@gmail.com>
2017-07-20 16:55:58 -0400

    deleting old files

--- old-352-652/germany-world-cup/jquery-1/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$("#legendhide").click(function(){
-		$(".legendbox").toggle();
-		$("#legendhide span").toggle();
-		$("#legendhide").toggleClass("btn-danger");
-	});
-});
-
-
